fbfc5dab86bf745c7731a95bd3dd28d09882f21c
Jessica Stokes <hello@jessicastokes.net>
2017-05-26 16:31:00 -0700

    Export ES2015-style regexps as well
    Closes #22.

--- script/inject-sequences.js
+++ script/inject-sequences.js
@@ -6,11 +6,18 @@ sequences.sort((a, b) => b.length - a.length);
 
 const trie = new Trie();
 trie.addAll(sequences);
-const sequencePattern = trie.toString();
-console.log(sequencePattern);
 
-for (const file of ['index.js', 'text.js']) {
+const emojiSequenceES5 = trie.toString();
+console.log(emojiSequenceES5);
+
+const emojiSequenceES2015 = trie.toString('u');
+console.log(emojiSequenceES2015);
+
+for (const file of ['index.js', 'text.js', 'es2015/index.js', 'es2015/text.js']) {
+	const emojiSequence = file.startsWith('es2015/') ? emojiSequenceES2015 : emojiSequenceES5;
+
 	const input = fs.readFileSync(file, 'utf8');
-	const output = input.replace('<% emojiSequence %>', sequencePattern);
+	const output = input.replace('<% emojiSequence %>', emojiSequence);
+
 	fs.writeFileSync(file, output);
 }
