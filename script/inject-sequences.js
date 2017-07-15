65ab14018e39898c77b837153bcc414dc81e6617
Jessica Stokes <hello@jessicastokes.net>
2017-07-14 17:33:36 -0700

    Deduplicate source files

--- script/inject-sequences.js
+++ script/inject-sequences.js
@@ -7,18 +7,17 @@ sequences.sort((a, b) => b.length - a.length);
 const trie = new Trie();
 trie.addAll(sequences);
 
-const emojiSequence = trie.toString();
-console.log(emojiSequence);
+const emojiSequenceES5 = trie.toString();
+console.log(emojiSequenceES5);
 
-const emojiSequenceUnicode = trie.toString('u');
-console.log(emojiSequenceUnicode);
+const emojiSequenceES2015 = trie.toString('u');
+console.log(emojiSequenceES2015);
 
 for (const file of ['index.js', 'text.js', 'es2015/index.js', 'es2015/text.js']) {
-	const input = fs.readFileSync(file, 'utf8');
+	const emojiSequence = file.startsWith('es2015/') ? emojiSequenceES2015 : emojiSequenceES5;
 
-	const output = input
-		.replace('<% emojiSequence %>', emojiSequence)
-		.replace('<% emojiSequenceUnicode %>', emojiSequenceUnicode);
+	const input = fs.readFileSync(file, 'utf8');
+	const output = input.replace('<% emojiSequence %>', emojiSequence);
 
 	fs.writeFileSync(file, output);
 }
