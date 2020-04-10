13a245bb8b24bb001dca14888ee753f743cf9b32
Mathias Bynens <mathias@qiwi.be>
2020-04-10 11:33:55 +0200

    Update to Unicode v13.0.0
    Ref. #66.

--- script/get-sequences.js
+++ script/get-sequences.js
@@ -1,13 +1,18 @@
 const properties = [
-	'Emoji_Flag_Sequence',
 	'Emoji_Keycap_Sequence',
-	'Emoji_Modifier_Sequence',
-	'Emoji_Tag_Sequence',
-	'Emoji_ZWJ_Sequence',
+	'RGI_Emoji_Flag_Sequence',
+	'RGI_Emoji_Modifier_Sequence',
+	'RGI_Emoji_Tag_Sequence',
+	'RGI_Emoji_ZWJ_Sequence',
 ];
-const sequences = properties.reduce((accumulator, property) => {
-	const array = require(`unicode-12.1.0/Sequence_Property/${property}/index.js`);
-	return [...accumulator, ...array];
-}, []).sort((a, b) => [...b].length - [...a].length);
+const set = new Set();
+for (const property of properties) {
+	const array = require(`unicode-13.0.0/Sequence_Property/${property}/index.js`);
+	for (const sequence of array) {
+		set.add(sequence);
+	}
+}
+
+const sequences = [...set].sort((a, b) => [...b].length - [...a].length);
 
 module.exports = sequences;
