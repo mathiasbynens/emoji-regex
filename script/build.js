b8d660f828b1f3f2b09d092590eeee9d58862f94
Mathias Bynens <mathias@qiwi.be>
2022-09-29 16:08:45 +0200

    Align coding style

--- script/build.js
+++ script/build.js
@@ -7,6 +7,6 @@ const output = input.replace('<% pattern %>', pattern) + '\n';
 fs.writeFileSync('./index.js', output);
 
 const inputModule = fs.readFileSync('./src/index.mjs', 'utf8').toString().trim();
-const outputModule = inputModule.replace("<% pattern %>", pattern) + "\n";
+const outputModule = inputModule.replace('<% pattern %>', pattern) + '\n';
 
 fs.writeFileSync('./index.mjs', outputModule);
